234fd1a7183a68c652d68bf5a76853d4dd798429
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-04-30 11:57:34 +0530

    refactor: streamline MyOperator webhook settings with computed fields and improved layout

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -6,20 +6,16 @@ class ResConfigSettings(models.TransientModel):
     myoperator_webhook_url = fields.Char(
         string='Webhook URL',
         readonly=True,
-        default=lambda self: self.env['ir.config_parameter'].sudo().get_param(
-            'web.base.url', '') + '/myoperator/webhook'
+        compute='_compute_webhook_url'
     )
     
     myoperator_webhook_secret = fields.Char(
         string='Webhook Secret',
         config_parameter='myoperator_webhook_integration.webhook_secret'
     )
-    
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        res['myoperator_webhook_url'] = self.env['ir.config_parameter'].sudo().get_param(
-            'web.base.url', '') + '/myoperator/webhook'
-        res['myoperator_webhook_secret'] = self.env['ir.config_parameter'].sudo().get_param(
-            'myoperator_webhook_integration.webhook_secret', '')
-        return res
+
+    @api.depends('company_id')
+    def _compute_webhook_url(self):
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url', '')
+        for record in self:
+            record.myoperator_webhook_url = f"{base_url}/myoperator/webhook"
